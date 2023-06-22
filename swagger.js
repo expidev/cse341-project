03fd61d0996b79a37634c3cc2c226d5c8031678f
Nasandratra Harivonjy <nasandratra.access4@gmail.com>
2023-06-21 22:31:28 -0700

    done

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
       title: 'My API',
       description: 'Contacts API for L04 assignment',
     },
-    host: 'https://nasproject-cse341.onrender.com',
+    host: 'nasproject-cse341.onrender.com',
     schemes: ['https'],
 };
 
